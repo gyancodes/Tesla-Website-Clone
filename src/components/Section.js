259c2c1cdd3083379c624fca0763eebd6dd4c891
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-06-21 10:11:59 +0530

    UI Changes

---
+++ src/components/Section.js
@@ -0,0 +1,62 @@
+/* eslint-disable react/jsx-no-undef */
+import React from 'react'
+import styled from 'styled-components'
+
+
+function Section() {
+    return (
+        <Wrap>
+            <Text>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </Text>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+
+                </RightButton>
+
+            </ButtonGroup>
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width:100vw;
+    height:100vh;
+    background-size:cover;
+    background-position:center;
+    background-repeat: no-repeat;
+    background-image:url('/images/model-3.jpg');
+
+    `
+
+
+const Text = styled.div`
+padding-top:15vh;
+text-align:center;
+
+`
+const ButtonGroup = styled.div`
+
+
+`
+
+const LeftButton = styled.div`
+background-color: rgba(23,26,32,0.8);
+height: 40px;
+width:256px;
+color:white;
+display:flex;
+
+
+
+`
+const RightButton = styled(LeftButton)`
+
+`
