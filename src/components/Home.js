1b44f7088980d5d13c6200b834f59ad6c95781f7
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-06-23 08:03:20 +0530

    updated header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import Section from './Section'
-
-function Home() {
+import AppsIcon from '@mui/icons-material/Apps';function Home() {
     return (
         // eslint-disable-next-line react/jsx-no-undef
         <Container>
+            <AppsIcon/>
             <Section 
             title="Model S" 
             description = "Order Online for Touchless Delivery"
